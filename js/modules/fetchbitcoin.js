19e985306db0ba09209ed83198c0cea2eace4afa
Pedro Henrique <phomendes@hotmail.com>
2022-08-08 14:06:37 -0300

    primeiro commit

---
+++ js/modules/fetchbitcoin.js
@@ -0,0 +1,10 @@
+export default function initFetchBitcoin() {
+  fetch('https://blockchain.info/ticker')
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcpreco = document.querySelector('.btc-preco');
+      btcpreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
+    }).catch((erro) => {
+      console.log(Error(erro));
+    });
+}
